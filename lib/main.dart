6749c3a2cc40563ce4ed02e202351b3ac7479c7e
50ShadesOfCode <kaktymail@gmail.com>
2022-04-22 14:40:23 +0300

    Remove linter from modules. Change app structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:fpmi_music_band/application.dart';
 
 void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FPMI Flutter Music Band',
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
+  runApp(const Application());
 }
