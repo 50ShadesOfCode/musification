4cd408cebc481627051af3b5c0fe4cbb22254ec7
Artyom Kaktysh <artsiomkaktysh@innowise-group.com>
2022-05-01 18:13:24 +0300

    Add shared_preferences.

--- data/lib/data.dart
+++ data/lib/data.dart
@@ -1,1 +1,3 @@
 library data;
+
+export 'package:data/src/di/data_di.dart';
---
+++ data/lib/src/prefs/prefs_provider.dart
@@ -0,0 +1,20 @@
+import 'package:shared_dependencies/sharedprefs.dart';
+
+class PrefsProvider {
+  late SharedPreferences _sharedPreferences;
+
+  static const String _keyIsFirstLaunch = 'isFirstLaunch';
+
+  Future<void> initializeSharedPreferences() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  Future<void> firstLaunch() async {
+    await _sharedPreferences.setBool(_keyIsFirstLaunch, true);
+  }
+
+  bool isFirstLaunch() {
+    final bool? isFirst = _sharedPreferences.getBool(_keyIsFirstLaunch);
+    return isFirst ?? false;
+  }
+}
