622f76107e787cd1ec63f40917a2ff3af4929b74
Artyom Kaktysh <artsiomkaktysh@innowise-group.com>
2022-05-05 18:52:38 +0300

    Add SF UI font.

--- core_ui/lib/src/app_fonts.dart
+++ core_ui/lib/src/app_fonts.dart
@@ -2,12 +2,60 @@ part of core_ui;
 
 class AppFonts {
   static const String _robotoFamily = 'Roboto';
-
+  static const String _sfuiFamily = 'SF UI Display';
   // Test TextStyle
-  static const TextStyle main = TextStyle(
+  static const TextStyle headline1 = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    color: AppColors.active,
+  );
+  static const TextStyle headline3 = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+    color: AppColors.active,
+  );
+  static const TextStyle headline2 = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    color: AppColors.active,
+  );
+  static const TextStyle bodyBold = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    color: AppColors.active,
+  );
+  static const TextStyle bodyRegular = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: AppColors.active,
+  );
+  static const TextStyle button = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    color: AppColors.active,
+  );
+  static const TextStyle googleButton = TextStyle(
     fontFamily: _robotoFamily,
     fontWeight: FontWeight.w500,
-    fontSize: 40,
+    fontSize: 14,
+    color: AppColors.active,
+  );
+  static const TextStyle caption = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: AppColors.active,
+  );
+  static const TextStyle tips = TextStyle(
+    fontFamily: _sfuiFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
     color: AppColors.active,
   );
 }
--- lib/feature/home_page.dart
+++ lib/feature/home_page.dart
@@ -22,7 +22,7 @@ class HomePage extends Page<void> {
         body: Center(
           child: Text(
             'Home',
-            style: AppFonts.main,
+            style: AppFonts.headline1,
           ),
         ),
       ),
