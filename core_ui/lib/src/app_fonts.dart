1da6b063a43f654183a6d45fb2e850958ebc3619
Aleksei Ushkov <aleksei.ushkov@innowise-group.com>
2022-05-10 13:45:07 +0300

    Add scaffoldBackgroundColor.

--- core_ui/lib/src/app_fonts.dart
+++ core_ui/lib/src/app_fonts.dart
@@ -2,40 +2,40 @@ part of core_ui;
 
 class AppFonts {
   static const String _robotoFamily = 'Roboto';
-  static const String _sfuiFamily = 'SF UI Display';
-  // Test TextStyle
+  static const String _sfUiFamily = 'SF UI Display';
+
   static const TextStyle headline1 = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w700,
     fontSize: 24,
     color: AppColors.active,
   );
   static const TextStyle headline3 = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w700,
     fontSize: 18,
     color: AppColors.active,
   );
   static const TextStyle headline2 = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w600,
     fontSize: 17,
     color: AppColors.active,
   );
   static const TextStyle bodyBold = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w700,
     fontSize: 14,
     color: AppColors.active,
   );
   static const TextStyle bodyRegular = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w400,
     fontSize: 14,
     color: AppColors.active,
   );
   static const TextStyle button = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w500,
     fontSize: 14,
     color: AppColors.active,
@@ -47,13 +47,13 @@ class AppFonts {
     color: AppColors.active,
   );
   static const TextStyle caption = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w400,
     fontSize: 14,
     color: AppColors.active,
   );
   static const TextStyle tips = TextStyle(
-    fontFamily: _sfuiFamily,
+    fontFamily: _sfUiFamily,
     fontWeight: FontWeight.w400,
     fontSize: 12,
     color: AppColors.active,
