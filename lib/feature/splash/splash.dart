1797d06ad19172011a3f194a0d80fe35401791cc
Aleksei Ushkov <aleksei.ushkov@innowise-group.com>
2022-05-12 16:16:47 +0300

    Unfinished splash.

---
+++ lib/feature/splash/splash.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fpmi_music_band/feature/splash/splash_page.dart';
+
+class Splash {
+  static /*Page<dynamic>*/ Scaffold page() => Scaffold()/*SplashPage()*/;
+}
