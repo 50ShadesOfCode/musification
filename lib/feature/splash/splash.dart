58c9b032f9655f1d0a69b77c984fb79f76f54270
Artyom Kaktysh <artsiomkaktysh@innowise-group.com>
2022-05-12 16:35:35 +0300

    Remove splash from branch.

--- lib/feature/splash/splash.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:fpmi_music_band/feature/splash/splash_page.dart';
-
-class Splash {
-  static /*Page<dynamic>*/ Scaffold page() => Scaffold()/*SplashPage()*/;
-}
