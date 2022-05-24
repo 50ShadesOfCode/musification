99899acf7838871230dc79265479bb1d31018491
Aleksei Ushkov <aleksei.ushkov@innowise-group.com>
2022-05-24 21:36:13 +0300

    Add WebView to last.fm/join.
    w javascript enabled & without exiting WebView

--- features/authorization/lib/authorization_form.dart
+++ features/authorization/lib/authorization_form.dart
@@ -1,6 +1,7 @@
 import 'package:authorization/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_dependencies/bloc.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AuthForm extends StatelessWidget {
 
@@ -11,9 +12,13 @@ class AuthForm extends StatelessWidget {
     return Scaffold(
       body: BlocBuilder<AuthBloc, AuthState>(
         builder: (BuildContext context, AuthState state) {
-          return const Center(
-            child: Text('Auth'),
-          );
+          return const SafeArea(
+            child: WebView(
+              initialUrl: 'https://www.last.fm/join',
+              javascriptMode: JavascriptMode.unrestricted,
+              // TODO: Close WebView after login
+            ),
+          ); // TODO: Add internet_connection exception
         },
       ),
     );
