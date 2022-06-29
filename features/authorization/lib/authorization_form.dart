6ed5c35e836db7c811c3e4c7259f4348fcfe5d48
Aleksei Ushkov <aleksei.ushkov@innowise-group.com>
2022-06-29 17:22:19 +0300

    Minor changes in UI

--- features/authorization/lib/authorization_form.dart
+++ features/authorization/lib/authorization_form.dart
@@ -46,85 +46,145 @@ class _AuthFormState extends State<AuthForm> {
           }
           return SafeArea(
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    width: 350,
-                    height: 40,
-                    child: TextField(
-                      controller: usernameController,
-                      onChanged: (String text) {
-                        username = text;
-                      },
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          borderSide: BorderSide.none,
-                        ),
-                        contentPadding: EdgeInsets.all(10),
-                        hintText: 'Username',
-                        fillColor: AppColors.secondary2,
-                        filled: true,
-                      ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset(AppImages.logoPngKey),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Container(
-                    width: 350,
-                    height: 40,
-                    child: TextField(
-                      controller: passwordController,
-                      onChanged: (String text) {
-                        password = text;
-                      },
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                        contentPadding: EdgeInsets.all(10),
-                        hintText: 'Password',
-                        fillColor: AppColors.secondary2,
-                        filled: true,
-                      ),
+                    const SizedBox(
+                      height: 40,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton(
-                    onPressed: () => <void>{
-                      BlocProvider.of<AuthBloc>(context).add(SignInEvent(
-                        username: username,
-                        password: password,
-                      )),
-                    },
-                    child: const Text('Sign In'),
-                  ),
-                  const SizedBox(
-                    height: 160,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Don\'t have an account?'),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      ElevatedButton(
-                        onPressed: () => <void>{
-                          BlocProvider.of<AuthBloc>(context).add(RegisterEvent())
+                    Container(
+                      width: 350,
+                      height: 40,
+                      child: TextField(
+                        controller: usernameController,
+                        onChanged: (String text) {
+                          username = text;
                         },
-                        child: const Text('Register'),
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                          hintText: 'Username',
+                          fillColor: AppColors.secondary2,
+                          filled: true,
+                        ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 128,
-                  ),
-                ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      width: 350,
+                      height: 40,
+                      child: TextField(
+                        controller: passwordController,
+                        onChanged: (String text) {
+                          password = text;
+                        },
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                          hintText: 'Password',
+                          fillColor: AppColors.secondary2,
+                          filled: true,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      width: 350,
+                      height: 40,
+                      child: TextButton(
+                        onPressed: () => <void>{
+                            BlocProvider.of<AuthBloc>(context).add(SignInEvent(
+                              username: username,
+                              password: password,
+                            )),
+                          },
+                        style: TextButton.styleFrom(
+                          backgroundColor: AppColors.accent,
+                          primary: AppColors.active,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )
+                        ),
+                        child: const Text('Sign In'),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 200,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          width: 150,
+                          child: Divider(
+                            thickness: 1,
+                            color: AppColors.secondary1,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'or',
+                          style: TextStyle(
+                            color: AppColors.secondary1,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                          width: 150,
+                          child: Divider(
+                            thickness: 1,
+                            color: AppColors.secondary1,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Don\'t have an account?',
+                          style: TextStyle(
+                            color: AppColors.secondary1,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () => <void>{
+                            BlocProvider.of<AuthBloc>(context).add(RegisterEvent())
+                          },
+                          style: TextButton.styleFrom(
+                            primary: AppColors.active,
+                          ),
+                          child: const Text(
+                            'Register',
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
               ),
             ),
           );
